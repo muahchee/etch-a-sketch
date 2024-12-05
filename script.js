9f7cccd843a57403e658655fd19220c20f6ba0c8
muahchee <187490843+muahchee@users.noreply.github.com>
2024-12-06 11:44:44 +1300

    Improve initial grid
    now the grid actually shows up. now i need to make the draw squares consistent

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const dimension = 16;
 
+let drawSquare;
+let row;
+let canvas;
+
 //create a single draw square
 function createDrawSquare(){
-    const drawSquare = document.createElement("div");
+    drawSquare = document.createElement("div");
     drawSquare.setAttribute("class", "draw-square");
 
 }
 
 //create a row of draw squares according to width dimension
 function createRow(){
-    const row = document.createElement("div");
-    row.setAttribute("clsss", "row");
+    row = document.createElement("div");
+    row.setAttribute("class", "row");
 
-    for(i = 1 ; i <= dimension ; i++){
+    for(let i = 1 ; i <= dimension ; i++){
         createDrawSquare();
         row.appendChild(drawSquare);
     }
@@ -20,9 +24,9 @@ function createRow(){
 
 //create full canvas (column of rows according to width dimensions)
 function createCanvas(){
-    const canvas = document.querySelector("#canvas");
+    canvas = document.querySelector("#canvas");
 
-    for(i = 1 ; i <= dimension ; i++){
+    for(let j = 1 ; j <= dimension ; j++){
         createRow();
         canvas.appendChild(row);
     }
